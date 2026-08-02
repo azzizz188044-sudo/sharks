99be11febdcf76401d6d2e4298ab213d583b29fb
ergashevaziz-dev <ergashevazizdev@gmail.com>
2026-08-02 19:30:02 +0500

    fixed

--- config/settings.py
+++ config/settings.py
@@ -128,9 +128,13 @@ import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
